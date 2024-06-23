60fbeb85e6366bacf6df78450679977e66d5a70c
Xembo <nishadvishal937@gmail.com>
2024-06-23 13:44:00 -0700

    working

--- src/APIs/api.js
+++ src/APIs/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://backend-task2do-1.onrender.com';
+const API_URL = "https://backend-task2do-1.onrender.com/tasks";
 
 export const fetchTasks = async () => {
   try {
